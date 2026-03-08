ed8f8d435de58cd7a1d5cb9178e0b8868a644e6f
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-03-09 00:18:44 +0700

    refactor(deploy): use hybrid output and clean up git index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wisnualfiannurashar.my.id',
-  output: 'server',
+  output: 'hybrid',
   integrations: [sitemap()],
   adapter: vercel({
     webAnalytics: true
