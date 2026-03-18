d13f865b0987b41650e102e0e26b80fe4f8ed16d
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-03-19 06:52:33 +0700

    fix(deploy): use relative base and explicit file format for maximum compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wisnu-alfian-nur-ashar.vercel.app',
+  base: './',
   output: 'static',
+  build: {
+    format: 'file'
+  },
   integrations: [sitemap()],
   // adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    base: './'
   }
 });
