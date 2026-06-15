b27d373f242c4b35598686a287c452b9911b8cc3
Wisnu Alfian Nur Ashar <wisnualfian117@gmail.com>
2026-06-15 19:04:32 +0700

    Fix build error by changing output to static (Astro 5/6 update)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import compress from 'astro-compress';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.wisnualfiannurashar.my.id',
-  output: 'hybrid',
+  output: 'static',
   build: {
     inlineStylesheets: 'always',
     format: 'file'
